a77618f8365005c5691130a459ad9e809548c72f
Adam Kwapisz <adam.kwapisz123@gmail.com>
2024-01-31 10:35:27 +0100

    https changed

--- frontend/server.js
+++ frontend/server.js
@@ -11,18 +11,16 @@ const port = process.env.PORT || 3000;
 
 const sslOptions = {
     pfx: fs.readFileSync('secure-markdown.pfx'),
-    passphrase: 'Haslo123@', // Set the password you used during the PKCS12 file creation
+    passphrase: 'Haslo123@',
 };
 
 app.prepare().then(() => {
     const server = express();
 
-    // Your existing Next.js routes
     server.all('*', (req, res) => {
         return handle(req, res);
     });
 
-    // HTTPS server
     https.createServer(sslOptions, server).listen(port, (err) => {
         if (err) throw err;
         console.log(`> Ready on https://localhost:${port}`);
